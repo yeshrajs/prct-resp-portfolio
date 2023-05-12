e2eadc250a346b924f22761f3b46cf492ad19572
yeshrajs <singhyeshr@gamil.com>
2023-05-13 00:53:49 +0530

    fixed overlay for nav

---
+++ dist/js/main.js
@@ -0,0 +1,34 @@
+// selecting DOM items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-brand');
+const navItem = document.querySelectorAll('.nav-item');
+
+// Set Initial state of menue
+  let showMenu = false;
+ menuBtn.addEventListener('click',toggleMenu);
+ 
+function toggleMenu(){
+if(!showMenu){
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItem.forEach(item => item.classList.add('show'
+    ));
+    // Set menue state
+    showMenu=true;
+
+} 
+else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItem.forEach(item => item.classList.remove('show'
+    ));
+    showMenu = false;
+}
+}
+// btnline not working properly
